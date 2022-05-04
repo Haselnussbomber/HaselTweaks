3cce41fbb8d6044d0ed9114e15f9be24f4c32926
Haselnussbomber <mail@haselnussbomber.de>
2022-05-04 20:26:02 +0200

    remove unused structs

--- Structs/PlayerState.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.InteropServices;
-
-namespace HaselTweaks.Structs;
-
-[StructLayout(LayoutKind.Explicit, Size = 0x788)]
-public unsafe struct PlayerState
-{
-    [FieldOffset(0x614)] public fixed byte WeeklyBonusOrderDataIds[16];
-}
