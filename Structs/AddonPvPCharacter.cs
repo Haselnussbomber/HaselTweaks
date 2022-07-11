292b3605e818d9a4456d5b4a910a0de5d4bdd80f
Haselnussbomber <mail@haselnussbomber.de>
2022-07-11 05:49:18 +0200

    support PvPCharacter window

---
+++ Structs/AddonPvPCharacter.cs
@@ -0,0 +1,41 @@
+using System.Runtime.InteropServices;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace HaselTweaks.Structs;
+
+// ctor 48 89 5C 24 ?? 57 48 83 EC 20 48 8B D9 E8 ?? ?? ?? ?? 48 8D 05 ?? ?? ?? ?? 48 8D 8B ?? ?? ?? ?? 48 89 03 E8 ?? ?? ?? ?? BA
+[StructLayout(LayoutKind.Explicit, Size = 0xC60)]
+public unsafe struct AddonPvPCharacter
+{
+    public const int NUM_CLASSES = 19;
+
+    [FieldOffset(0)] public AtkUnitBase AtkUnitBase;
+    [FieldOffset(0x240)] public ClassEntriesArray ClassEntries;
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x28)]
+    public struct ClassEntry
+    {
+        [FieldOffset(0x00)] public AtkComponentBase* Base;
+        [FieldOffset(0x08)] public AtkTextNode* Name;
+        [FieldOffset(0x10)] public AtkTextNode* Level;
+        [FieldOffset(0x18)] public AtkImageNode* Icon;
+        [FieldOffset(0x20)] public AtkImageNode* UnkImage;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Size = NUM_CLASSES * 0x28)]
+    public struct ClassEntriesArray
+    {
+        private fixed byte data[NUM_CLASSES * 0x28];
+        public ClassEntry* this[int i]
+        {
+            get
+            {
+                if (i < 0 || i > NUM_CLASSES) return null;
+                fixed (byte* p = data)
+                {
+                    return (ClassEntry*)(p + sizeof(ClassEntry) * i);
+                }
+            }
+        }
+    }
+}
--- Tweaks/CharacterClassSwitcher.cs
+++ Tweaks/CharacterClassSwitcher.cs
@@ -22,16 +22,29 @@ public unsafe class CharacterClassSwitcher : Tweak
         public bool DisableTooltips = false;
     }
 
-    /*
+    private bool TooltipPatchApplied = false;
+
+    /* Address for AddonCharacterClass Tooltip Patch
+
         83 FD 14         cmp     ebp, 14h
         48 8B 6C 24 ??   mov     rbp, [rsp+68h+arg_8]
         7D 69            jge     short loc_140EB06A1     <- replacing this with a jmp rel8
-    
-        completely skips the whole if () {...} block, by jumping regardless of cmp result
+
+       completely skips the whole if () {...} block, by jumping regardless of cmp result
      */
     [Signature("83 FD 14 48 8B 6C 24 ?? 7D 69")]
     private IntPtr TooltipAddress { get; init; }
-    private bool TooltipPatchApplied = false;
+
+    /* Address for AddonPvPCharacter Tooltip Patch
+
+        48 8D 4C 24 ??   lea     rcx, [rsp+68h+var_28]   <- replacing this with a jmp rel8
+        E8 ?? ?? ?? ??   call    Component::GUI::AtkTooltipArgs_ctor
+        ...
+
+        completely skips the tooltip code, by jumping to the end of the function
+     */
+    [Signature("48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8B 83 ?? ?? ?? ?? 48 8B CF 0F B7 9F")]
+    private IntPtr PvPTooltipAddress { get; init; }
 
     public override void Enable()
     {
@@ -53,12 +66,14 @@ public unsafe class CharacterClassSwitcher : Tweak
         if (enable && !TooltipPatchApplied)
         {
             MemoryUtils.ReplaceRaw(TooltipAddress + 8, new byte[] { 0xEB }); // jmp rel8
+            MemoryUtils.ReplaceRaw(PvPTooltipAddress, new byte[] { 0xEB, 0x63 }); // jmp rel8
 
             TooltipPatchApplied = true;
         }
         else if (!enable && TooltipPatchApplied)
         {
             MemoryUtils.ReplaceRaw(TooltipAddress + 8, new byte[] { 0x7D }); // jge rel8
+            MemoryUtils.ReplaceRaw(PvPTooltipAddress, new byte[] { 0x48, 0x8D }); // original bytes (see signature)
 
             TooltipPatchApplied = false;
         }
@@ -69,15 +84,30 @@ public unsafe class CharacterClassSwitcher : Tweak
     private Hook<OnSetupDelegate> OnSetupHook { get; init; } = null!;
     private delegate IntPtr OnSetupDelegate(AddonCharacterClass* addon, int a2);
 
+    // AddonCharacterClass_OnUpdate (vf49)
+    [AutoHook, Signature("4C 8B DC 53 55 56 57 41 55 41 56", DetourName = nameof(OnUpdate))]
+    private Hook<OnUpdateDelegate> OnUpdateHook { get; init; } = null!;
+    private delegate void OnUpdateDelegate(AddonCharacterClass* addon, NumberArrayData** numberArrayData, StringArrayData** stringArrayData);
+
     // AddonCharacterClass_ReceiveEvent (vf2)
     [AutoHook, Signature("48 89 5C 24 ?? 57 48 83 EC 20 48 8B D9 4D 8B D1", DetourName = nameof(OnEvent))]
     private Hook<ReceiveEventDelegate> ReceiveEventHook { get; init; } = null!;
     private delegate IntPtr ReceiveEventDelegate(AddonCharacterClass* addon, AtkEventType eventType, int eventParam, AtkEvent* atkEvent, IntPtr a5);
 
-    // AddonCharacterClass_OnUpdate (vf49)
-    [AutoHook, Signature("4C 8B DC 53 55 56 57 41 55 41 56", DetourName = nameof(OnUpdate))]
-    private Hook<OnUpdateDelegate> OnUpdateHook { get; init; } = null!;
-    private delegate void OnUpdateDelegate(AddonCharacterClass* addon, NumberArrayData** numberArrayData, StringArrayData** stringArrayData);
+    // AddonPvPCharacter_OnSetup (first fn in vf46)
+    [AutoHook, Signature("E8 ?? ?? ?? ?? 48 8B 83 ?? ?? ?? ?? 45 33 FF 41 8B CF 48 85 C0 74 07 48 8B 88 ?? ?? ?? ?? 45 33 C0", DetourName = nameof(OnPvPSetup))]
+    private Hook<OnPvPSetupDelegate> OnPvPSetupHook { get; init; } = null!;
+    private delegate IntPtr OnPvPSetupDelegate(AddonPvPCharacter* addon);
+
+    // AddonPvPCharacter_OnUpdate (second fn in vf49)
+    [AutoHook, Signature("48 8B C4 48 89 58 20 55 56 57 41 56 41 57 48 81 EC", DetourName = nameof(OnPvPUpdate))]
+    private Hook<OnPvPUpdateDelegate> OnPvPUpdateHook { get; init; } = null!;
+    private delegate void OnPvPUpdateDelegate(AddonPvPCharacter* addon, NumberArrayData** numberArrayData, StringArrayData** stringArrayData);
+
+    // AddonPvPCharacter_ReceiveEvent (vf2)
+    [AutoHook, Signature("48 89 5C 24 ?? 57 48 83 EC 30 0F B7 C2 4D 8B D1 83 C0 FD", DetourName = nameof(OnPvPEvent))]
+    private Hook<PvPReceiveEventDelegate> PvPReceiveEventHook { get; init; } = null!;
+    private delegate IntPtr PvPReceiveEventDelegate(AddonPvPCharacter* addon, AtkEventType eventType, int eventParam, AtkEvent* atkEvent, IntPtr a5);
 
     [Signature("E8 ?? ?? ?? ?? 0F BF 94 1F")]
     private PlaySoundEffectDelegate PlaySoundEffect { get; init; } = null!;
@@ -91,6 +121,7 @@ public unsafe class CharacterClassSwitcher : Tweak
     private IntPtr OnSetup(AddonCharacterClass* addon, int a2)
     {
         var result = OnSetupHook!.Original(addon, a2);
+        var eventListener = &addon->AtkUnitBase.AtkEventListener;
 
         for (var i = 0; i < AddonCharacterClass.NUM_CLASSES; i++)
         {
@@ -103,8 +134,8 @@ public unsafe class CharacterClassSwitcher : Tweak
             var collisionNode = (AtkCollisionNode*)node->UldManager.RootNode;
             if (collisionNode == null) continue;
 
-            collisionNode->AtkResNode.AddEvent(AtkEventType.MouseClick, (uint)i + 2, &addon->AtkUnitBase.AtkEventListener, &collisionNode->AtkResNode, false);
-            collisionNode->AtkResNode.AddEvent(AtkEventType.InputReceived, (uint)i + 2, &addon->AtkUnitBase.AtkEventListener, &collisionNode->AtkResNode, false);
+            collisionNode->AtkResNode.AddEvent(AtkEventType.MouseClick, (uint)i + 2, eventListener, null, false);
+            collisionNode->AtkResNode.AddEvent(AtkEventType.InputReceived, (uint)i + 2, eventListener, null, false);
         }
 
         return result;
@@ -142,72 +173,147 @@ public unsafe class CharacterClassSwitcher : Tweak
     {
         // skip events for tabs
         if (eventParam < 2)
-            goto OriginalCode;
+            goto OriginalReceiveEventHookCode;
 
         var node = addon->BaseComponentNodes[eventParam - 2];
-        if (node == null)
-            goto OriginalCode;
-
-        var ownerNode = node->OwnerNode;
-        if (ownerNode == null)
-            goto OriginalCode;
+        if (node == null || node->OwnerNode == null)
+            goto OriginalReceiveEventHookCode;
 
         var imageNode = (AtkImageNode*)node->UldManager.SearchNodeById(4);
         if (imageNode == null)
-            goto OriginalCode;
+            goto OriginalReceiveEventHookCode;
 
         // if job is unlocked, it has full alpha
         var isUnlocked = imageNode->AtkResNode.Color.A == 255;
         if (!isUnlocked)
-            goto OriginalCode;
+            goto OriginalReceiveEventHookCode;
 
-        var isClick =
-            (eventType == AtkEventType.MouseClick || eventType == AtkEventType.ButtonClick) ||
-            (eventType == AtkEventType.InputReceived && GamepadUtils.IsPressed(GamepadUtils.GamepadBinding.Accept));
-
+        // special handling for crafters
         if (IsCrafter(eventParam - 2))
         {
+            var isClick =
+                (eventType == AtkEventType.MouseClick || eventType == AtkEventType.ButtonClick) ||
+                (eventType == AtkEventType.InputReceived && GamepadUtils.IsPressed(GamepadUtils.GamepadBinding.Accept));
+
             if (isClick && !Service.KeyState[VirtualKey.SHIFT])
             {
                 SwitchClassJob(8 + (uint)eventParam - 22);
                 return IntPtr.Zero;
             }
         }
-        else
+        else if (ProcessEvents(node->OwnerNode, imageNode, eventType))
+            return IntPtr.Zero;
+
+OriginalReceiveEventHookCode:
+        return ReceiveEventHook!.Original(addon, eventType, eventParam, atkEvent, a5);
+    }
+
+    private IntPtr OnPvPSetup(AddonPvPCharacter* addon)
+    {
+        var result = OnPvPSetupHook!.Original(addon);
+        var eventListener = &addon->AtkUnitBase.AtkEventListener;
+
+        for (var i = 0; i < AddonPvPCharacter.NUM_CLASSES; i++)
         {
-            if (isClick)
-            {
-                var textureInfo = imageNode->PartsList->Parts[imageNode->PartId].UldAsset;
-                if (textureInfo == null || textureInfo->AtkTexture.Resource == null)
-                    return ReceiveEventHook!.Original(addon, eventType, eventParam, atkEvent, a5);
+            var entry = addon->ClassEntries[i];
+            if (entry == null || entry->Base == null) continue;
 
-                var iconId = textureInfo->AtkTexture.Resource->IconID;
-                if (iconId <= 62100)
-                    return ReceiveEventHook!.Original(addon, eventType, eventParam, atkEvent, a5);
+            var collisionNode = (AtkCollisionNode*)entry->Base->UldManager.RootNode;
+            if (collisionNode == null) continue;
 
-                // yes, you see correctly. the iconId is 62100 + ClassJob RowId :)
-                var classJobId = iconId - 62100;
-
-                SwitchClassJob((uint)classJobId);
-
-                return IntPtr.Zero; // handled
-            }
-            else if (eventType == AtkEventType.MouseOver)
-            {
-                ownerNode->AtkResNode.AddBlue = 16;
-                ownerNode->AtkResNode.AddGreen = 16;
-                ownerNode->AtkResNode.AddRed = 16;
-            }
-            else if (eventType == AtkEventType.MouseOut)
-            {
-                ownerNode->AtkResNode.AddBlue = 0;
-                ownerNode->AtkResNode.AddGreen = 0;
-                ownerNode->AtkResNode.AddRed = 0;
-            }
+            collisionNode->AtkResNode.AddEvent(AtkEventType.MouseClick, (uint)i | 0x10000, eventListener, null, false);
+            collisionNode->AtkResNode.AddEvent(AtkEventType.InputReceived, (uint)i | 0x10000, eventListener, null, false);
         }
 
-OriginalCode:
-        return ReceiveEventHook!.Original(addon, eventType, eventParam, atkEvent, a5);
+        return result;
+    }
+
+    private void OnPvPUpdate(AddonPvPCharacter* addon, NumberArrayData** numberArrayData, StringArrayData** stringArrayData)
+    {
+        OnPvPUpdateHook!.Original(addon, numberArrayData, stringArrayData);
+
+        for (var i = 0; i < AddonPvPCharacter.NUM_CLASSES; i++)
+        {
+            var entry = addon->ClassEntries[i];
+            if (entry == null || entry->Base == null || entry->Icon == null) continue;
+
+            var collisionNode = (AtkCollisionNode*)entry->Base->UldManager.RootNode;
+            if (collisionNode == null) continue;
+
+            // if job is unlocked, it has full alpha
+            var isUnlocked = entry->Icon->AtkResNode.Color.A == 255;
+
+            if (isUnlocked)
+                collisionNode->AtkResNode.Flags_2 |= 1 << 20; // add Cursor Pointer flag
+            else
+                collisionNode->AtkResNode.Flags_2 &= ~(uint)(1 << 20); // remove Cursor Pointer flag
+        }
+    }
+
+    private IntPtr OnPvPEvent(AddonPvPCharacter* addon, AtkEventType eventType, int eventParam, AtkEvent* atkEvent, IntPtr a5)
+    {
+        if ((eventParam & 0xFFFF0000) != 0x10000)
+            goto OriginalPvPReceiveEventCode;
+
+        var entryId = eventParam & 0x0000FFFF;
+        if (entryId is < 0 or > AddonPvPCharacter.NUM_CLASSES)
+            goto OriginalPvPReceiveEventCode;
+
+        var entry = addon->ClassEntries[entryId];
+
+        if (entry == null || entry->Base == null || entry->Base->OwnerNode == null || entry->Icon == null)
+            goto OriginalPvPReceiveEventCode;
+
+        // if job is unlocked, it has full alpha
+        var isUnlocked = entry->Icon->AtkResNode.Color.A == 255;
+        if (!isUnlocked)
+            goto OriginalPvPReceiveEventCode;
+
+        if (ProcessEvents(entry->Base->OwnerNode, entry->Icon, eventType))
+            return IntPtr.Zero;
+
+OriginalPvPReceiveEventCode:
+        return PvPReceiveEventHook!.Original(addon, eventType, eventParam, atkEvent, a5);
+    }
+
+    /// <returns>Boolean whether original code should be skipped (true) or not (false)</returns>
+    private bool ProcessEvents(AtkComponentNode* componentNode, AtkImageNode* imageNode, AtkEventType eventType)
+    {
+        var isClick =
+            eventType == AtkEventType.MouseClick ||
+            (eventType == AtkEventType.InputReceived && GamepadUtils.IsPressed(GamepadUtils.GamepadBinding.Accept));
+
+        if (isClick)
+        {
+            var textureInfo = imageNode->PartsList->Parts[imageNode->PartId].UldAsset;
+            if (textureInfo == null || textureInfo->AtkTexture.Resource == null)
+                return false;
+
+            var iconId = textureInfo->AtkTexture.Resource->IconID;
+            if (iconId <= 62100)
+                return false;
+
+            // yes, you see correctly. the iconId is 62100 + ClassJob RowId :)
+            var classJobId = iconId - 62100;
+
+            SwitchClassJob((uint)classJobId);
+
+            return true; // handled
+        }
+        else if (eventType == AtkEventType.MouseOver)
+        {
+            componentNode->AtkResNode.AddBlue = 16;
+            componentNode->AtkResNode.AddGreen = 16;
+            componentNode->AtkResNode.AddRed = 16;
+        }
+        else if (eventType == AtkEventType.MouseOut)
+        {
+            componentNode->AtkResNode.AddBlue = 0;
+            componentNode->AtkResNode.AddGreen = 0;
+            componentNode->AtkResNode.AddRed = 0;
+        }
+
+        return false;
     }
 
     private void SwitchClassJob(uint classJobId)
@@ -232,7 +338,7 @@ OriginalCode:
         if (selectedGearset.Index == -1)
         {
             // TODO: localize
-            Service.Chat.PrintError($"Couldn't find a suitable gearset.");
+            Service.Chat.PrintError("Couldn't find a suitable gearset.");
             return;
         }
 
