70c0ef6ba0449b06c284c76d3493f3e2e36111cb
Haselnussbomber <mail@haselnussbomber.de>
2023-01-27 18:01:03 +0100

    cache AtkCollisionManager.Instance

--- Structs/AtkCollisionManager.cs
+++ Structs/AtkCollisionManager.cs
@@ -6,7 +6,16 @@ namespace HaselTweaks.Structs;
 [StructLayout(LayoutKind.Explicit)]
 public unsafe struct AtkCollisionManager
 {
-    public static AtkCollisionManager* Instance => *(AtkCollisionManager**)((IntPtr)AtkStage.GetSingleton() + 0x30);
+    private static nint instance;
+    public static AtkCollisionManager* Instance
+    {
+        get
+        {
+            if (instance == 0)
+                instance = *(nint*)((IntPtr)AtkStage.GetSingleton() + 0x30);
+            return (AtkCollisionManager*)instance;
+        }
+    }
 
     [FieldOffset(0x08)] public AtkUnitBase* IntersectingAddon;
     [FieldOffset(0x10)] public AtkCollisionNode* IntersectingCollisionNode;
