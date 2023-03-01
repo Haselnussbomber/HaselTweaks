92c6bb6a4d38de55e434522f8ae8b3571f78686c
Haselnussbomber <mail@haselnussbomber.de>
2023-03-01 19:04:51 +0100

    quotation marks around a sig

--- Structs/RaptureShellModule.cs
+++ Structs/RaptureShellModule.cs
@@ -7,7 +7,7 @@ public unsafe partial struct RaptureShellModule
 {
     public static RaptureShellModule* Instance => (RaptureShellModule*)Framework.Instance()->GetUiModule()->GetRaptureShellModule();
 
-    [FieldOffset(0x1200)] public uint Flags; // set by E8 ?? ?? ?? ?? 83 FE 1C
+    [FieldOffset(0x1200)] public uint Flags; // set by "E8 ?? ?? ?? ?? 83 FE 1C"
 
     public bool IsTextCommandUnavailable => (Flags & 1) != 0;
 }
