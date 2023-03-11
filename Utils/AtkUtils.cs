d8b33cddfc0e06111de2d7bb0a065ba17518fc98
Haselnussbomber <mail@haselnussbomber.de>
2023-03-11 02:06:12 +0100

    add out overloads of AtkUtils

--- Utils/AtkUtils.cs
+++ Utils/AtkUtils.cs
@@ -13,7 +13,20 @@ public static unsafe class AtkUtils
     public static AtkUnitBase* GetAddon(string name, int index = 1)
         => (AtkUnitBase*)Service.GameGui.GetAddonByName(name, index);
 
-    public static T* GetAddon<T>(string name, int index = 1) => (T*)GetAddon(name, index);
+    public static bool GetAddon(string name, int index, out AtkUnitBase* addon)
+        => (addon = (AtkUnitBase*)Service.GameGui.GetAddonByName(name, index)) != null;
+
+    public static bool GetAddon(string name, out AtkUnitBase* addon)
+        => GetAddon(name, 1, out addon);
+
+    public static T* GetAddon<T>(string name, int index = 1)
+        => (T*)GetAddon(name, index);
+
+    public static bool GetAddon<T>(string name, int index, out T* addon)
+        => (addon = (T*)GetAddon(name, index)) != null;
+
+    public static bool GetAddon<T>(string name, out T* addon)
+        => GetAddon(name, 1, out addon);
 
     #endregion
 
@@ -21,16 +34,51 @@ public static unsafe class AtkUtils
 
     public static AtkUnitBase* GetAddon(ushort id)
         => IsAddonReady(id) ? AtkStage.GetSingleton()->RaptureAtkUnitManager->GetAddonById(id) : null;
-    public static AtkUnitBase* GetAddon(uint id) => GetAddon((ushort)id);
+
+    public static bool GetAddon(ushort id, out AtkUnitBase* addon)
+        => (addon = GetAddon(id)) != null;
+
+    public static AtkUnitBase* GetAddon(uint id)
+        => GetAddon((ushort)id);
+
+    public static bool GetAddon(uint id, out AtkUnitBase* addon)
+        => GetAddon((ushort)id, out addon);
+
     public static AtkUnitBase* GetAddon(AgentInterface* agent)
         => agent != null && agent->IsAgentActive() ? GetAddon((ushort)agent->GetAddonID()) : null;
-    public static AtkUnitBase* GetAddon(AgentId id) => GetAddon(GetAgent(id));
 
-    public static T* GetAddon<T>(ushort id) => (T*)GetAddon(id);
-    public static T* GetAddon<T>(uint id) => GetAddon<T>((ushort)id);
+    public static bool GetAddon(AgentInterface* agent, out AtkUnitBase* addon)
+        => (addon = GetAddon(agent)) != null;
+
+    public static AtkUnitBase* GetAddon(AgentId id)
+        => GetAddon(GetAgent(id));
+
+    public static bool GetAddon(AgentId id, out AtkUnitBase* addon)
+        => GetAddon(GetAgent(id), out addon);
+
+    public static T* GetAddon<T>(ushort id)
+        => (T*)GetAddon(id);
+
+    public static bool GetAddon<T>(ushort id, out T* addon)
+        => (addon = (T*)GetAddon(id)) != null;
+
+    public static T* GetAddon<T>(uint id)
+        => GetAddon<T>((ushort)id);
+
+    public static bool GetAddon<T>(uint id, out T* addon)
+        => GetAddon((ushort)id, out addon);
+
     public static T* GetAddon<T>(AgentInterface* agent)
         => agent != null && agent->IsAgentActive() ? GetAddon<T>((ushort)agent->GetAddonID()) : null;
-    public static T* GetAddon<T>(AgentId id) => GetAddon<T>(GetAgent(id));
+
+    public static bool GetAddon<T>(AgentInterface* agent, out T* addon)
+        => (addon = GetAddon<T>(agent)) != null;
+
+    public static T* GetAddon<T>(AgentId id)
+        => GetAddon<T>(GetAgent(id));
+
+    public static bool GetAddon<T>(AgentId id, out T* addon)
+        => GetAddon(GetAgent(id), out addon);
 
     #endregion
 
@@ -51,8 +99,14 @@ public static unsafe class AtkUtils
     public static AgentInterface* GetAgent(uint id)
         => AgentModule.Instance()->GetAgentByInternalID(id);
 
+    public static bool GetAgent(uint id, out AgentInterface* agent)
+        => (agent = GetAgent(id)) != null;
+
     public static AgentInterface* GetAgent(AgentId id)
         => AgentModule.Instance()->GetAgentByInternalId(id);
+
+    public static bool GetAgent(AgentId id, out AgentInterface* agent)
+        => (agent = GetAgent(id)) != null;
 
     /*
      * too slow
@@ -63,7 +117,11 @@ public static unsafe class AtkUtils
     }
     */
 
-    public static T* GetAgent<T>(AgentId id) => (T*)GetAgent(id);
+    public static T* GetAgent<T>(AgentId id)
+        => (T*)GetAgent(id);
+
+    public static bool GetAgent<T>(AgentId id, out T* agent)
+        => (agent = GetAgent<T>(id)) != null;
 
     #endregion
 
@@ -72,11 +130,20 @@ public static unsafe class AtkUtils
     public static AtkResNode* GetNode(AtkUnitBase* addon, uint nodeId)
         => addon == null ? null : addon->UldManager.SearchNodeById(nodeId);
 
+    public static bool GetNode(AtkUnitBase* addon, uint nodeId, out AtkResNode* node)
+        => (node = GetNode(addon, nodeId)) != null;
+
     public static T* GetNode<T>(AtkUnitBase* addon, uint nodeId)
         => (T*)GetNode(addon, nodeId);
 
-    public static T* GetNode<T>(AtkComponentBase* node, uint nodeId)
-        => node == null ? null : (T*)node->UldManager.SearchNodeById(nodeId);
+    public static bool GetNode<T>(AtkUnitBase* addon, uint nodeId, out T* node)
+        => (node = GetNode<T>(addon, nodeId)) != null;
+
+    public static T* GetNode<T>(AtkComponentBase* component, uint nodeId)
+        => component == null ? null : (T*)component->UldManager.SearchNodeById(nodeId);
+
+    public static bool GetNode<T>(AtkComponentBase* component, uint nodeId, out T* node)
+        => (node = GetNode<T>(component, nodeId)) != null;
 
     #endregion
 
