251bdd8032cc3a0ced62ccc44b1948f48621cb0f
Haselnussbomber <mail@haselnussbomber.de>
2023-03-24 12:40:51 +0100

    use OnAddonOpen in MaterialAllocation

--- Tweaks/MaterialAllocation.cs
+++ Tweaks/MaterialAllocation.cs
@@ -48,10 +48,13 @@ public unsafe partial class MaterialAllocation : Tweak
         nextMJIGatheringNoteBookItemId = 0;
     }
 
-    // vf48 = OnOpen?
-    [SigHook("BA ?? ?? ?? ?? E9 ?? ?? ?? ?? CC CC CC CC CC CC 40 57 48 83 EC 20 48 8B F9 85 D2 7E 51")]
-    public nint AddonMJICraftMaterialConfirmation_vf48(AddonMJICraftMaterialConfirmation* addon, int numAtkValues, nint atkValues)
+    public override unsafe void OnAddonOpen(string addonName, AtkUnitBase* unitbase)
     {
+        if (addonName != "MJICraftMaterialConfirmation")
+            return;
+
+        var addon = (AddonMJICraftMaterialConfirmation*)unitbase;
+
         if (Config.SaveLastSelectedTab)
         {
             if (Config.LastSelectedTab > 2)
@@ -69,7 +72,11 @@ public unsafe partial class MaterialAllocation : Tweak
             }
         }
 
-        return AddonMJICraftMaterialConfirmation_vf48Hook.Original(addon, numAtkValues, atkValues);
+        if (addon->ItemList != null && addon->ItemList->AtkComponentBase.OwnerNode != null)
+        {
+            //addon->ItemList->AtkComponentBase.OwnerNode->AtkResNode.AddEvent(31, 9901, (AtkEventListener*)addon, null, false); // MouseDown
+            addon->ItemList->AtkComponentBase.OwnerNode->AtkResNode.AddEvent(32, 9902, (AtkEventListener*)addon, null, false); // MouseUp
+        }
     }
 
     [SigHook("40 53 48 83 EC 20 48 8B 51 28 48 8B D9 8B 0A 83 E9 01 74 39")]
@@ -91,17 +98,6 @@ public unsafe partial class MaterialAllocation : Tweak
         }
 
         return ret;
-    }
-
-    [SigHook("48 89 5C 24 ?? 55 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24 ?? 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 45 17 BA ?? ?? ?? ?? 49 8B D8")]
-    public void AddonMJICraftMaterialConfirmation_OnSetup(AddonMJICraftMaterialConfirmation* addon, int numAtkValues, AtkValue* atkValues)
-    {
-        AddonMJICraftMaterialConfirmation_OnSetupHook.Original(addon, numAtkValues, atkValues);
-        if (addon->ItemList != null && addon->ItemList->AtkComponentBase.OwnerNode != null)
-        {
-            //addon->ItemList->AtkComponentBase.OwnerNode->AtkResNode.AddEvent(31, 9901, (AtkEventListener*)addon, null, false); // MouseDown
-            addon->ItemList->AtkComponentBase.OwnerNode->AtkResNode.AddEvent(32, 9902, (AtkEventListener*)addon, null, false); // MouseUp
-        }
     }
 
     [SigHook("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC 60 41 8D 40 FF")]
