ae61db56b8ea63b5a6c607dafe43740f7793903e
Haselnussbomber <mail@haselnussbomber.de>
2023-04-28 01:35:53 +0200

    catch exceptions in CustomChatTimestamp

--- Tweaks/CustomChatTimestamp.cs
+++ Tweaks/CustomChatTimestamp.cs
@@ -37,16 +37,26 @@ public unsafe partial class CustomChatTimestamp : Tweak
     [SigHook("E8 ?? ?? ?? ?? 48 8B D0 48 8B CB E8 ?? ?? ?? ?? 4C 8D 87")]
     private byte* FormatAddon(nint a1, ulong addonRowId, ulong value)
     {
-        if (addonRowId != 7840) return FormatAddonHook.Original(a1, addonRowId, value);
+        if (addonRowId == 7840)
+        {
+            try
+            {
+                var str = (Utf8String*)(a1 + 0x9C0);
+                var time = DateTime.UnixEpoch.AddSeconds(value).ToLocalTime();
+                var formatted = time.ToString(Config.Format);
 
-        var str = (Utf8String*)(a1 + 0x9C0);
-        var time = DateTime.UnixEpoch.AddSeconds(value).ToLocalTime();
-        var formatted = time.ToString(Config.Format);
+                MemoryHelper.WriteString((nint)str->StringPtr, formatted);
+                str->BufUsed = formatted.Length + 1;
+                str->StringLength = formatted.Length;
 
-        MemoryHelper.WriteString((nint)str->StringPtr, formatted + "\0");
-        str->BufUsed = formatted.Length + 1;
-        str->StringLength = formatted.Length;
+                return str->StringPtr;
+            }
+            catch (Exception e)
+            {
+                Error(e, "Error formatting Chat Timestamp");
+            }
+        }
 
-        return str->StringPtr;
+        return FormatAddonHook.Original(a1, addonRowId, value);
     }
 }
