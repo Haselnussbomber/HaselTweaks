7507184f2e5d04d8eb0694dd91c073bfd15235cc
Haselnussbomber <mail@haselnussbomber.de>
2023-05-01 02:08:14 +0200

    update NameFormatter

--- Caches/StringCache.cs
+++ Caches/StringCache.cs
@@ -4,8 +4,6 @@ using Dalamud.Memory;
 using FFXIVClientStructs.FFXIV.Client.System.Framework;
 using HaselTweaks.Structs;
 using Lumina.Excel;
-using NameFormatterIdConverter = HaselTweaks.Structs.NameFormatter.IdConverter;
-using NameFormatterReplacer = HaselTweaks.Structs.NameFormatter.Replacer;
 
 namespace HaselTweaks.Caches;
 
@@ -13,70 +11,89 @@ public sealed unsafe partial class StringCache
 {
     private static Dictionary<uint, string> QuestCache = new();
     private static Dictionary<uint, string> AddonCache = new();
-    private static Dictionary<(NameFormatterReplacer, NameFormatterIdConverter, uint), string> NameCache = new(); // NameCache[(replacer, idConverter, id)]
+    private static Dictionary<(NameFormatter.Placeholder, NameFormatter.IdConverter, uint), string> NameCache = new(); // NameCache[(placeholder, idConverter, id)]
     private static Dictionary<(string, uint, string), string> SheetCache = new(); // SheetCache[(sheetName, rowId, columnName)]
 
-    public static string FormatName(NameFormatterReplacer replacer, NameFormatterIdConverter idConverter, uint id)
+    public static string? FormatName(NameFormatter.Placeholder placeholder, NameFormatter.IdConverter idConverter, uint id)
     {
-        var key = (replacer, idConverter, id);
+        var key = (placeholder, idConverter, id);
 
         if (!NameCache.TryGetValue(key, out var value))
         {
-            var ptr = NameFormatter.Format(replacer, id, idConverter, 1);
+            var ptr = NameFormatter.Format(placeholder, id, idConverter, 1);
             if (ptr != 0)
             {
                 value = MemoryHelper.ReadSeStringNullTerminated(ptr).ToString();
+
+                if (string.IsNullOrWhiteSpace(value))
+                    return null;
+
                 NameCache.Add(key, value);
             }
         }
 
-        return value ?? $"[{Enum.GetName(typeof(NameFormatterIdConverter), idConverter)}#{id}]";
+        return value;
     }
 
     public static string GetItemName(uint id)
-        => FormatName(NameFormatterReplacer.Item, NameFormatterIdConverter.None, id);
-
-    public static string GetActionName(uint id)
-        => FormatName(NameFormatterReplacer.ActStr, NameFormatterIdConverter.Action, id);
+        => FormatName(NameFormatter.Placeholder.Item, NameFormatter.IdConverter.Item, id) ?? $"[Item#{id}]";
 
     public static string GetBNpcName(uint id)
-        => FormatName(NameFormatterReplacer.ObjStr, NameFormatterIdConverter.BNpc, id);
+        => FormatName(NameFormatter.Placeholder.ObjStr, NameFormatter.IdConverter.ObjStr_BNpcName, id) ?? $"[BNpcName#{id}]";
 
     public static string GetENpcResidentName(uint id)
-        => FormatName(NameFormatterReplacer.ObjStr, NameFormatterIdConverter.ENpcResident, id);
+        => FormatName(NameFormatter.Placeholder.ObjStr, NameFormatter.IdConverter.ObjStr_ENpcResident, id) ?? $"[ENpcResident#{id}]";
 
-    public static string GetEventActionName(uint id)
-        => FormatName(NameFormatterReplacer.ActStr, NameFormatterIdConverter.EventAction, id);
+    public static string GetTreasureName(uint id)
+        => FormatName(NameFormatter.Placeholder.ObjStr, NameFormatter.IdConverter.ObjStr_Treasure, id) ?? $"[Treasure#{id}]";
 
-    public static string GetEObjName(uint id)
-        => FormatName(NameFormatterReplacer.ActStr, NameFormatterIdConverter.EObj, id);
+    public static string GetAetheryteName(uint id)
+        => FormatName(NameFormatter.Placeholder.ObjStr, NameFormatter.IdConverter.ObjStr_Aetheryte, id) ?? $"[Aetheryte#{id}]";
 
     public static string GetGatheringPointName(uint id)
-        => FormatName(NameFormatterReplacer.ObjStr, NameFormatterIdConverter.GatheringPoint, id);
+        => FormatName(NameFormatter.Placeholder.ObjStr, NameFormatter.IdConverter.ObjStr_GatheringPointName, id) ?? $"[GatheringPointName#{id}]";
 
-    public static string GetMainCommandName(uint id)
-        => FormatName(NameFormatterReplacer.ActStr, NameFormatterIdConverter.MainCommand, id);
+    public static string GetEObjName(uint id)
+        => FormatName(NameFormatter.Placeholder.ObjStr, NameFormatter.IdConverter.ObjStr_EObjName, id) ?? $"[EObjName#{id}]";
 
     public static string GetCompanionName(uint id)
-        => FormatName(NameFormatterReplacer.ObjStr, NameFormatterIdConverter.Companion, id);
+        => FormatName(NameFormatter.Placeholder.ObjStr, NameFormatter.IdConverter.ObjStr_Companion, id) ?? $"[Companion#{id}]";
+
+    public static string GetTraitName(uint id)
+        => FormatName(NameFormatter.Placeholder.ActStr, NameFormatter.IdConverter.ActStr_Trait, id) ?? $"[Trait#{id}]";
+
+    public static string GetActionName(uint id)
+        => FormatName(NameFormatter.Placeholder.ActStr, NameFormatter.IdConverter.ActStr_Action, id) ?? $"[Action#{id}]";
+
+    public static string GetEventActionName(uint id)
+        => FormatName(NameFormatter.Placeholder.ActStr, NameFormatter.IdConverter.ActStr_EventAction, id) ?? $"[EventAction#{id}]";
+
+    public static string GetGeneralActionName(uint id)
+        => FormatName(NameFormatter.Placeholder.ActStr, NameFormatter.IdConverter.ActStr_GeneralAction, id) ?? $"[GeneralAction#{id}]";
+
+    public static string GetBuddyActionName(uint id)
+        => FormatName(NameFormatter.Placeholder.ActStr, NameFormatter.IdConverter.ActStr_BuddyAction, id) ?? $"[BuddyAction#{id}]";
+
+    public static string GetMainCommandName(uint id)
+        => FormatName(NameFormatter.Placeholder.ActStr, NameFormatter.IdConverter.ActStr_MainCommand, id) ?? $"[MainCommand#{id}]";
 
     public static string GetCraftActionName(uint id)
-        => FormatName(NameFormatterReplacer.ActStr, NameFormatterIdConverter.CraftAction, id);
+        => FormatName(NameFormatter.Placeholder.ActStr, NameFormatter.IdConverter.ActStr_CraftAction, id) ?? $"[CraftAction#{id}]";
 
     public static string GetPetActionName(uint id)
-        => FormatName(NameFormatterReplacer.ActStr, NameFormatterIdConverter.PetAction, id);
+        => FormatName(NameFormatter.Placeholder.ActStr, NameFormatter.IdConverter.ActStr_PetAction, id) ?? $"[PetAction#{id}]";
 
     public static string GetCompanyActionName(uint id)
-        => FormatName(NameFormatterReplacer.ActStr, NameFormatterIdConverter.CompanyAction, id);
+        => FormatName(NameFormatter.Placeholder.ActStr, NameFormatter.IdConverter.ActStr_CompanyAction, id) ?? $"[CompanyAction#{id}]";
 
     public static string GetMountName(uint id)
-        => FormatName(NameFormatterReplacer.ActStr, NameFormatterIdConverter.Mount, id);
+        => FormatName(NameFormatter.Placeholder.ActStr, NameFormatter.IdConverter.ActStr_Mount, id) ?? $"[Mount#{id}]";
 
     public static string GetBgcArmyActionName(uint id)
-        => FormatName(NameFormatterReplacer.ActStr, NameFormatterIdConverter.BgcArmyAction, id);
+        => FormatName(NameFormatter.Placeholder.ActStr, NameFormatter.IdConverter.ActStr_BgcArmyAction, id) ?? $"[BgcArmyAction#{id}]";
 
     public static string GetOrnamentName(uint id)
-        => FormatName(NameFormatterReplacer.ActStr, NameFormatterIdConverter.Ornament, id);
+        => FormatName(NameFormatter.Placeholder.ActStr, NameFormatter.IdConverter.ActStr_Ornament, id) ?? $"[Ornament#{id}]";
 
     public static string? GetAddonText(uint rowId)
     {
