26d5e300b5be16a4c925568318d45bc043181004
Haselnussbomber <mail@haselnussbomber.de>
2023-06-01 15:22:36 +0200

    use RaptureTextModule.FormatAddonText2
    it doesn't really matter, but it's one less sig to scan

--- Tweaks/EnhancedMaterialList.cs
+++ Tweaks/EnhancedMaterialList.cs
@@ -405,10 +405,6 @@ public unsafe partial class EnhancedMaterialList : Tweak
     public readonly IsGatheringPointTypeOffDelegate IsGatheringPointRare = null!;
     public delegate bool IsGatheringPointTypeOffDelegate(byte gatheringPointType);
 
-    [Signature("E8 ?? ?? ?? ?? 41 B0 07")]
-    public readonly FormatAddonTextDelegate FormatAddonText = null!;
-    public delegate byte* FormatAddonTextDelegate(RaptureTextModule* module, uint id, int value);
-
     [Signature("E8 ?? ?? ?? ?? 4C 8B 05 ?? ?? ?? ?? 48 8D 8C 24 ?? ?? ?? ?? 48 8B D0 E8 ?? ?? ?? ?? 8B 4E 08")]
     public readonly GetGatheringPointNameDelegate GetGatheringPointName = null!;
     public delegate byte* GetGatheringPointNameDelegate(RaptureTextModule** module, byte gatheringType, byte gatheringPointType);
@@ -441,7 +437,7 @@ public unsafe partial class EnhancedMaterialList : Tweak
 
         var levelText = gatheringPointBase.GatheringLevel == 1
             ? raptureTextModule->GetAddonText(242) // "Lv. ???"
-            : FormatAddonText(raptureTextModule, 35, gatheringPointBase.GatheringLevel);
+            : raptureTextModule->FormatAddonText2(35, gatheringPointBase.GatheringLevel, 0);
         var space = MemoryUtils.FromString(" ");
         var gatheringPointName = GetGatheringPointName(
             &raptureTextModule,
