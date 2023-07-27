acd2b335ae64e64b77dc0a836e3105ab0229d844
Haselnussbomber <mail@haselnussbomber.de>
2023-07-27 04:28:54 +0200

    update commands (add /whatbarding)

--- Tweaks/Commands.cs
+++ Tweaks/Commands.cs
@@ -1,11 +1,17 @@
 using System.Linq;
+using Dalamud.Game.ClientState.Objects.Enums;
 using Dalamud.Game.Command;
 using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
 using Dalamud.Memory;
-using FFXIVClientStructs.FFXIV.Client.Game.Object;
+using Dalamud.Utility;
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using HaselTweaks.Caches;
 using Lumina.Excel.GeneratedSheets;
+using GameObject = FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject;
+using ObjectKind = Dalamud.Game.ClientState.Objects.Enums.ObjectKind;
 
 namespace HaselTweaks.Tweaks;
 
@@ -19,6 +25,7 @@ public unsafe class Commands : Tweak
 
     private const string ItemLinkCommand = "/itemlink";
     private const string WhatMountCommand = "/whatmount";
+    private const string WhatBardingCommand = "/whatbarding";
 
     public class Configuration
     {
@@ -27,6 +34,9 @@ public unsafe class Commands : Tweak
 
         [ConfigField(Label = WhatMountCommand, Description = $"Target a player and execute the command to see what mount\nyour target is riding and which item teaches this mount.", OnChange = nameof(OnConfigChange))]
         public bool EnableWhatMountCommand = true;
+
+        [ConfigField(Label = WhatBardingCommand, Description = $"Target a players chocobo companion and execute the command to see\nwhat barding it is wearing.", OnChange = nameof(OnConfigChange))]
+        public bool EnableWhatBardingCommand = true;
     }
 
     private static void OnConfigChange()
@@ -66,6 +76,16 @@ public unsafe class Commands : Tweak
                 ShowInHelp = true
             });
         }
+
+        if (Config.EnableWhatBardingCommand)
+        {
+            Service.Commands.RemoveHandler(WhatBardingCommand);
+            Service.Commands.AddHandler(WhatBardingCommand, new CommandInfo(OnWhatBardingCommand)
+            {
+                HelpMessage = $"Usage: {WhatBardingCommand}",
+                ShowInHelp = true
+            });
+        }
     }
 
     private static void DisableCommands(bool removeAll = false)
@@ -78,6 +98,11 @@ public unsafe class Commands : Tweak
         if (!Config.EnableWhatMountCommand || removeAll)
         {
             Service.Commands.RemoveHandler(WhatMountCommand);
+        }
+
+        if (!Config.EnableWhatBardingCommand || removeAll)
+        {
+            Service.Commands.RemoveHandler(WhatBardingCommand);
         }
     }
 
@@ -97,30 +122,24 @@ public unsafe class Commands : Tweak
         var item = Service.Data.GetExcelSheet<Item>()!.GetRow(id);
         if (item == null)
         {
-            Service.Chat.PrintError($"Item #{id} not found");
+            Service.Chat.PrintError($"Item {id} not found");
             return;
         }
 
-        var fgColor = (ushort)(549 + (item.Rarity - 1) * 2);
-        var glowColor = (ushort)(fgColor + 1);
-
-        var sb = new SeStringBuilder()
-            .AddText($"Item #{id}: ")
-            .AddUiForeground(fgColor)
-            .AddUiGlow(glowColor)
-            .AddItemLink(item.RowId, false)
-            .AddUiForeground(500)
-            .AddUiGlow(501)
-            .AddText(SeIconChar.LinkMarker.ToIconString() + " ")
-            .AddUiForegroundOff()
-            .AddUiGlowOff()
-            .AddText(item.Name)
-            .Add(new RawPayload(new byte[] { 0x02, 0x27, 0x07, 0xCF, 0x01, 0x01, 0x01, 0xFF, 0x01, 0x03 })) // LinkTerminator
-            .Add(new RawPayload(new byte[] { 0x02, 0x13, 0x02, 0xEC, 0x03 })); // SaintCoinach says 0x13 is named Color, but idk
+        var itemLink = new SeStringBuilder()
+                    .AddUiForeground(SeIconChar.LinkMarker.ToIconString() + " ", 500)
+                    .Append(MemoryHelper.ReadSeStringNullTerminated((nint)RaptureTextModule.Instance()->FormatAddonText2(2021, (int)item.RowId, 0)))
+                    .Build();
 
         Service.Chat.PrintChat(new XivChatEntry
         {
-            Message = sb.BuiltString,
+            Message = new SeStringBuilder()
+                    .AddUiForeground("\uE078 ", 32)
+                    .AddText("Item ")
+                    .AddUiForeground(id.ToString(), 1)
+                    .AddText(": ")
+                    .Append(itemLink)
+                    .Build(),
             Type = XivChatType.Echo
         });
     }
@@ -130,31 +149,31 @@ public unsafe class Commands : Tweak
         var target = Service.TargetManager.Target;
         if (target == null)
         {
-            Service.Chat.PrintError($"No target.");
+            Service.Chat.PrintError("No target.");
+            return;
+        }
+
+        if (target.ObjectKind != ObjectKind.Player)
+        {
+            Service.Chat.PrintError("Target is not a player.");
             return;
         }
 
         var targetGameObject = (GameObject*)target.Address;
-        if (targetGameObject->ObjectKind != (byte)ObjectKind.Pc)
-        {
-            Service.Chat.PrintError($"Target is not a player.");
-            return;
-        }
-
         if (targetGameObject->ObjectIndex + 1 > Service.ObjectTable.Length)
         {
-            Service.Chat.PrintError($"Could not get mount object: not inside object table.");
+            Service.Chat.PrintError("Error: mount game object index out of bounds.");
             return;
         }
 
         var mountObject = Service.ObjectTable[targetGameObject->ObjectIndex + 1];
-        if (mountObject == null || (byte)mountObject.ObjectKind != (byte)ObjectKind.Mount)
+        if (mountObject == null || mountObject.ObjectKind != ObjectKind.MountType)
         {
-            Service.Chat.PrintError($"Target is not mounted.");
+            Service.Chat.PrintError("Target is not mounted.");
             return;
         }
 
-        var modelChara = MemoryHelper.Read<uint>(mountObject.Address + 0x1B4);
+        var modelChara = ((Character*)mountObject.Address)->CharacterData.ModelCharaId;
 
         var MountSheet = Service.Data.GetExcelSheet<Mount>()!;
         var ItemSheet = Service.Data.GetExcelSheet<Item>()!;
@@ -168,9 +187,11 @@ public unsafe class Commands : Tweak
 
         if (mountRow == null)
         {
-            Service.Chat.PrintError($"Mount not found.");
+            Service.Chat.PrintError("Mount not found.");
             return;
         }
+
+        var name = StringCache.GetMountName(mountRow.RowId);
 
         var itemActionRowId = (
             from row in ItemActionSheet
@@ -182,7 +203,11 @@ public unsafe class Commands : Tweak
         {
             Service.Chat.PrintChat(new XivChatEntry
             {
-                Message = $"Mount: {mountRow.Singular}",
+                Message = new SeStringBuilder()
+                    .AddUiForeground("\uE078 ", 32)
+                    .AddText("Mount: ")
+                    .AddUiForeground(name, 1)
+                    .Build(),
                 Type = XivChatType.Echo
             });
             return;
@@ -198,32 +223,138 @@ public unsafe class Commands : Tweak
         {
             Service.Chat.PrintChat(new XivChatEntry
             {
-                Message = $"Mount: {mountRow.Singular}",
+                Message = new SeStringBuilder()
+                    .AddUiForeground("\uE078 ", 32)
+                    .AddText("Mount: ")
+                    .AddUiForeground(name, 1)
+                    .Build(),
                 Type = XivChatType.Echo
             });
             return;
         }
 
-        var fgColor = (ushort)(549 + (itemRow.Rarity - 1) * 2);
-        var glowColor = (ushort)(fgColor + 1);
+        var sesb = new SeStringBuilder()
+            .AddUiForeground("\uE078 ", 32);
 
-        var sb = new SeStringBuilder()
-            .AddText($"Mount {mountRow.Singular} learned by ")
-            .AddUiForeground(fgColor)
-            .AddUiGlow(glowColor)
-            .AddItemLink(itemRow.RowId, false)
-            .AddUiForeground(500)
-            .AddUiGlow(501)
-            .AddText(SeIconChar.LinkMarker.ToIconString() + " ")
-            .AddUiForegroundOff()
-            .AddUiGlowOff()
-            .AddText(itemRow.Name)
-            .Add(new RawPayload(new byte[] { 0x02, 0x27, 0x07, 0xCF, 0x01, 0x01, 0x01, 0xFF, 0x01, 0x03 })) // LinkTerminator
-            .Add(new RawPayload(new byte[] { 0x02, 0x13, 0x02, 0xEC, 0x03 })); // SaintCoinach says 0x13 is named Color, but idk
+        var itemLink = new SeStringBuilder()
+                    .AddUiForeground(SeIconChar.LinkMarker.ToIconString() + " ", 500)
+                    .Append(MemoryHelper.ReadSeStringNullTerminated((nint)RaptureTextModule.Instance()->FormatAddonText2(2021, (int)itemRow.RowId, 0)))
+                    .Build();
+
+        switch (Service.ClientState.ClientLanguage)
+        {
+            case Dalamud.ClientLanguage.German:
+                sesb.AddText("Reittier ")
+                    .AddUiForeground(name, 1)
+                    .AddText(" gelehrt von ")
+                    .Append(itemLink);
+                break;
+            case Dalamud.ClientLanguage.French:
+                sesb.AddText("Monture ")
+                    .AddUiForeground(name, 1)
+                    .AddText(" enseignée par ")
+                    .Append(itemLink);
+                break;
+            case Dalamud.ClientLanguage.Japanese:
+                sesb.Append(itemLink)
+                    .AddText(" によって教えられた ")
+                    .AddUiForeground(name, 1)
+                    .AddText("のマウント");
+                break;
+            default:
+                sesb.AddText("Mount ")
+                    .AddUiForeground(name, 1)
+                    .AddText(" taught by ")
+                    .AddUiForeground(SeIconChar.LinkMarker.ToIconString() + " ", 500)
+                    .Append(itemLink);
+                break;
+        }
 
         Service.Chat.PrintChat(new XivChatEntry
         {
-            Message = sb.BuiltString,
+            Message = sesb.Build(),
+            Type = XivChatType.Echo
+        });
+    }
+
+    private static void OnWhatBardingCommand(string command, string arguments)
+    {
+        var target = Service.TargetManager.Target;
+        if (target == null)
+        {
+            Service.Chat.PrintError("No target.");
+            return;
+        }
+
+        if (target.ObjectKind != ObjectKind.BattleNpc || target.SubKind != (byte)BattleNpcSubKind.Chocobo)
+        {
+            Service.Chat.PrintError("Target is not a chocobo.");
+            return;
+        }
+
+        var targetCharacter = (Character*)target.Address;
+        var BuddyEquipSheet = Service.Data.GetExcelSheet<BuddyEquip>()!;
+
+        var topRow = (
+            from _row in BuddyEquipSheet
+            where _row.ModelTop == targetCharacter->DrawData.Head.Value
+            select _row
+        ).FirstOrDefault();
+
+        var bodyRow = (
+            from _row in BuddyEquipSheet
+            where _row.ModelBody == targetCharacter->DrawData.Top.Value
+            select _row
+        ).FirstOrDefault();
+
+        var legsRow = (
+            from _row in BuddyEquipSheet
+            where _row.ModelLegs == targetCharacter->DrawData.Feet.Value
+            select _row
+        ).FirstOrDefault();
+
+        var stain = Service.Data.GetExcelSheet<Stain>()!.GetRow(targetCharacter->DrawData.Legs.Stain)!;
+        var name = MemoryHelper.ReadStringNullTerminated((nint)targetCharacter->GameObject.Name);
+
+        var sesb = new SeStringBuilder()
+            .AddUiForeground("\uE078 ", 32);
+
+        switch (Service.ClientState.ClientLanguage)
+        {
+            case Dalamud.ClientLanguage.German:
+                sesb.AddText("Aussehen von ")
+                    .AddUiForeground(name, 1)
+                    .AddText(":");
+                break;
+            case Dalamud.ClientLanguage.French:
+                sesb.AddText("Apparence de ")
+                    .AddUiForeground(name, 1)
+                    .AddText(":");
+                break;
+            case Dalamud.ClientLanguage.Japanese:
+                sesb.AddUiForeground(name, 1)
+                    .AddText(" の外見：");
+                break;
+            default:
+                sesb.AddText("Appearance of ")
+                    .AddUiForeground(name, 1)
+                    .AddText(":");
+                break;
+        }
+
+        sesb.Add(NewLinePayload.Payload)
+            .AddText($"  {StringCache.GetAddonText(4987)}: ")
+            .Append(MemoryHelper.ReadSeStringNullTerminated((nint)RaptureTextModule.Instance()->FormatAddonText2(4986, (int)stain.RowId, 0))) // stain name
+            .Add(NewLinePayload.Payload)
+            .AddText($"  {StringCache.GetAddonText(4991)}: {topRow?.Name.ToDalamudString().ToString() ?? StringCache.GetAddonText(4994)}")
+            .Add(NewLinePayload.Payload)
+            .AddText($"  {StringCache.GetAddonText(4992)}: {bodyRow?.Name.ToDalamudString().ToString() ?? StringCache.GetAddonText(4994)}")
+            .Add(NewLinePayload.Payload)
+            .AddText($"  {StringCache.GetAddonText(4993)}: {legsRow?.Name.ToDalamudString().ToString() ?? StringCache.GetAddonText(4994)}");
+
+        Service.Chat.PrintChat(new XivChatEntry
+        {
+            Message = sesb.Build(),
             Type = XivChatType.Echo
         });
     }
