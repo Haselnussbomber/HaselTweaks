06cdace66596ee06770b84ff71d997f463f0f28a
Haselnussbomber <mail@haselnussbomber.de>
2023-10-05 20:38:06 +0200

    avoid goto in EnhancedExpBar

--- HaselTweaks/Tweaks/EnhancedExpBar.cs
+++ HaselTweaks/Tweaks/EnhancedExpBar.cs
@@ -23,9 +23,7 @@ public unsafe partial class EnhancedExpBar : Tweak
         Default,
         PvPSeriesBar,
         CompanionBar,
-
-        // Disabled because data is only available once loaded into the island. Sadge.
-        // SanctuaryBar
+        // No SanctuaryBar, because data is only available on the island
     }
 
     public class Configuration
@@ -140,187 +138,211 @@ public unsafe partial class EnhancedExpBar : Tweak
     [VTableHook<AddonExp>((int)AtkUnitBaseVfs.OnRequestedUpdate)]
     private nint AddonExp_OnRequestedUpdate(AddonExp* addon, NumberArrayData** numberArrayData, StringArrayData** stringArrayData)
     {
+        var ret = AddonExp_OnRequestedUpdateHook!.OriginalDisposeSafe(addon, numberArrayData, stringArrayData);
+
         if (!_isEnabled)
-            goto OriginalOnRequestedUpdate;
+            return ret;
 
         var gaugeBarNode = GetNode<AtkComponentNode>(&addon->AtkUnitBase, 6);
         if (gaugeBarNode == null)
-            goto OriginalOnRequestedUpdate;
+            return ret;
 
         var gaugeBar = (Structs.AtkComponentGaugeBar*)gaugeBarNode->Component;
         if (gaugeBar == null)
-            goto OriginalOnRequestedUpdate;
+            return ret;
 
         var nineGridNode = GetNode<AtkNineGridNode>(gaugeBarNode->Component, 4);
         if (nineGridNode == null)
-            goto OriginalOnRequestedUpdate;
+            return ret;
 
         if (Service.ClientState.LocalPlayer == null)
-            goto OriginalOnRequestedUpdateWithColorReset;
+        {
+            ResetColor(nineGridNode);
+            return ret;
+        }
 
         if (Service.ClientState.LocalPlayer.ClassJob.GameData == null)
-            goto OriginalOnRequestedUpdateWithColorReset;
+        {
+            ResetColor(nineGridNode);
+            return ret;
+        }
 
         var leftText = GetNode<AtkTextNode>(&addon->AtkUnitBase, 4);
         if (leftText == null)
-            goto OriginalOnRequestedUpdateWithColorReset;
-
-        var ret = AddonExp_OnRequestedUpdateHook!.OriginalDisposeSafe(addon, numberArrayData, stringArrayData);
-
-        string job, levelLabel, level;
-        uint requiredExperience;
+        {
+            ResetColor(nineGridNode);
+            return ret;
+        }
 
         // --- forced bars in certain locations
 
         if (Config.ForceCompanionBar && UIState.Instance()->Buddy.Companion.ObjectID != 0xE0000000)
-            goto CompanionBar;
+        {
+            HandleCompanionBar(nineGridNode, gaugeBar, leftText);
+            return ret;
+        }
 
-        if (Config.ForcePvPSeriesBar && GameMain.IsInPvPArea()) // TODO: only when PvP Series is active
-            goto PvPBar;
+        if (Config.ForcePvPSeriesBar && GameMain.IsInPvPArea())
+        {
+            HandlePvPBar(nineGridNode, gaugeBar, leftText);
+            return ret;
+        }
 
         if (Config.ForceSanctuaryBar && GameMain.Instance()->CurrentTerritoryIntendedUseId == 49)
-            goto SanctuaryBar;
+        {
+            HandleSanctuaryBar(nineGridNode, gaugeBar, leftText);
+            return ret;
+        }
 
         // --- max level overrides
 
         if (Service.ClientState.LocalPlayer.Level == PlayerState.Instance()->MaxLevel)
         {
             if (Config.MaxLevelOverride == MaxLevelOverrideType.PvPSeriesBar)
-                goto PvPBar;
+            {
+                HandlePvPBar(nineGridNode, gaugeBar, leftText);
+                return ret;
+            }
 
             if (Config.MaxLevelOverride == MaxLevelOverrideType.CompanionBar)
-                goto CompanionBar;
-
-            //if (Config.MaxLevelOverride == MaxLevelOverrideType.SanctuaryBar)
-            //    goto SanctuaryBar;
+            {
+                HandleCompanionBar(nineGridNode, gaugeBar, leftText);
+                return ret;
+            }
         }
 
         // nothing matches so let it fetch fresh data
         addon->ClassJob--;
         addon->RequiredExp--;
-
-        _isUpdatePending = false;
-
-        goto OriginalOnRequestedUpdateWithColorReset;
-
-PvPBar:
-        {
-            var pvpProfile = PvPProfile.Instance();
-            if (pvpProfile == null || pvpProfile->IsLoaded != 0x01)
-                goto OriginalOnRequestedUpdateWithColorReset;
-
-            if (pvpProfile->SeriesCurrentRank > GetRowCount<PvPSeriesLevel>() - 1)
-                goto OriginalOnRequestedUpdateWithColorReset;
-
-            var claimedRank = pvpProfile->GetSeriesClaimedRank();
-            var currentRank = pvpProfile->GetSeriesCurrentRank();
-
-            job = Service.ClientState.LocalPlayer.ClassJob.GameData.Abbreviation;
-            levelLabel = (GetAddonText(14860) ?? "Series Level").Trim().Replace(":", "");
-            var rank = currentRank > 30 ? 30 : currentRank; // 30 = Series Max Rank, hopefully in the future too
-            level = rank.ToString().Aggregate("", (str, chr) => str + (char)(SeIconChar.Number0 + byte.Parse(chr.ToString())));
-            var star = currentRank > claimedRank ? '*' : ' ';
-            requiredExperience = GetRow<PvPSeriesLevel>(currentRank)!.Unknown0;
-
-            leftText->SetText($"{job}  {levelLabel} {level}{star}   {pvpProfile->SeriesExperience}/{requiredExperience}");
-
-            gaugeBar->SetSecondaryValue(0); // rested experience bar
-
-            // max value is set to 10000 in AddonExp_OnSetup and we won't change that, so adjust
-            gaugeBar->SetValue((uint)(pvpProfile->SeriesExperience / (float)requiredExperience * 10000), 0, false);
-
-            if (!Config.DisableColorChanges)
-            {
-                // trying to make it look like the xp bar in the PvP Profile window and failing miserably. eh, good enough
-                nineGridNode->AtkResNode.MultiplyRed = 65;
-                nineGridNode->AtkResNode.MultiplyGreen = 35;
-            }
-            else
-            {
-                ResetColor(nineGridNode);
-            }
-
-            _isUpdatePending = false;
-            return ret;
-        }
-
-CompanionBar:
-        {
-            var buddy = UIState.Instance()->Buddy;
-            if (buddy.Rank > GetRowCount<BuddyRank>() - 1)
-                goto OriginalOnRequestedUpdateWithColorReset;
-
-            var currentRank = buddy.Rank;
-
-            job = Service.ClientState.LocalPlayer.ClassJob.GameData.Abbreviation;
-            levelLabel = (GetAddonText(4968) ?? "Rank").Trim().Replace(":", "");
-            var rank = currentRank > 20 ? 20 : currentRank;
-            level = rank.ToString().Aggregate("", (str, chr) => str + (char)(SeIconChar.Number0 + byte.Parse(chr.ToString())));
-            requiredExperience = GetRow<BuddyRank>(currentRank)!.ExpRequired;
-
-            var xpText = requiredExperience == 0 ? "" : $"   {buddy.CurrentXP}/{requiredExperience}";
-            leftText->SetText($"{job}  {levelLabel} {level}{xpText}");
-
-            gaugeBar->SetSecondaryValue(0); // rested experience bar
-
-            // max value is set to 10000 in AddonExp_OnSetup and we won't change that, so adjust
-            gaugeBar->SetValue((uint)(buddy.CurrentXP / (float)requiredExperience * 10000), 0, false);
-
-            ResetColor(nineGridNode);
-
-            _isUpdatePending = false;
-            return ret;
-        }
-
-SanctuaryBar:
-        {
-            var mjiManager = MJIManager.Instance();
-            if (mjiManager == null)
-                goto OriginalOnRequestedUpdateWithColorReset;
-
-            if (mjiManager->IslandState.CurrentRank > GetRowCount<MJIRank>() - 1)
-                goto OriginalOnRequestedUpdateWithColorReset;
-
-            job = Config.SanctuaryBarHideJob ? "" : Service.ClientState.LocalPlayer.ClassJob.GameData.Abbreviation + "  ";
-            levelLabel = (GetAddonText(14252) ?? "Sanctuary Rank").Trim().Replace(":", "");
-            level = mjiManager->IslandState.CurrentRank.ToString().Aggregate("", (str, chr) => str + (char)(SeIconChar.Number0 + byte.Parse(chr.ToString())));
-            requiredExperience = GetRow<MJIRank>(mjiManager->IslandState.CurrentRank)!.ExpToNext;
-
-            var expStr = mjiManager->IslandState.CurrentXP.ToString();
-            var reqExpStr = requiredExperience.ToString();
-            if (requiredExperience == 0)
-            {
-                expStr = reqExpStr = "--";
-            }
-
-            leftText->SetText($"{job}{levelLabel} {level}   {expStr}/{reqExpStr}");
-
-            gaugeBar->SetSecondaryValue(0); // rested experience bar
-
-            // max value is set to 10000 in AddonExp_OnSetup and we won't change that, so adjust
-            gaugeBar->SetValue((uint)(mjiManager->IslandState.CurrentXP / (float)requiredExperience * 10000), 0, false);
-
-            if (!Config.DisableColorChanges)
-            {
-                // blue seems nice.. just like the sky ^_^
-                nineGridNode->AtkResNode.MultiplyRed = 25;
-                nineGridNode->AtkResNode.MultiplyGreen = 60;
-                nineGridNode->AtkResNode.MultiplyBlue = 255;
-            }
-            else
-            {
-                ResetColor(nineGridNode);
-            }
-
-            _isUpdatePending = false;
-            return ret;
-        }
-
-OriginalOnRequestedUpdateWithColorReset:
         ResetColor(nineGridNode);
 
-OriginalOnRequestedUpdate:
-        return AddonExp_OnRequestedUpdateHook!.OriginalDisposeSafe(addon, numberArrayData, stringArrayData);
+        _isUpdatePending = false;
+        return ret;
+    }
+
+    private void HandleCompanionBar(AtkNineGridNode* nineGridNode, Structs.AtkComponentGaugeBar* gaugeBar, AtkTextNode* leftText)
+    {
+        var buddy = UIState.Instance()->Buddy;
+        if (buddy.Rank > GetRowCount<BuddyRank>() - 1)
+        {
+            ResetColor(nineGridNode);
+            return;
+        }
+
+        var currentRank = buddy.Rank;
+
+        var job = Service.ClientState.LocalPlayer!.ClassJob.GameData!.Abbreviation;
+        var levelLabel = (GetAddonText(4968) ?? "Rank").Trim().Replace(":", "");
+        var rank = currentRank > 20 ? 20 : currentRank;
+        var level = rank.ToString().Aggregate("", (str, chr) => str + (char)(SeIconChar.Number0 + byte.Parse(chr.ToString())));
+        var requiredExperience = GetRow<BuddyRank>(currentRank)!.ExpRequired;
+
+        var xpText = requiredExperience == 0 ? "" : $"   {buddy.CurrentXP}/{requiredExperience}";
+        leftText->SetText($"{job}  {levelLabel} {level}{xpText}");
+
+        gaugeBar->SetSecondaryValue(0); // rested experience bar
+
+        // max value is set to 10000 in AddonExp_OnSetup and we won't change that, so adjust
+        gaugeBar->SetValue((uint)(buddy.CurrentXP / (float)requiredExperience * 10000), 0, false);
+
+        ResetColor(nineGridNode);
+
+        _isUpdatePending = false;
+    }
+
+    private void HandlePvPBar(AtkNineGridNode* nineGridNode, Structs.AtkComponentGaugeBar* gaugeBar, AtkTextNode* leftText)
+    {
+        var pvpProfile = PvPProfile.Instance();
+        if (pvpProfile == null || pvpProfile->IsLoaded != 0x01)
+        {
+            ResetColor(nineGridNode);
+            return;
+        }
+
+        if (pvpProfile->SeriesCurrentRank > GetRowCount<PvPSeriesLevel>() - 1)
+        {
+            ResetColor(nineGridNode);
+            return;
+        }
+
+        var claimedRank = pvpProfile->GetSeriesClaimedRank();
+        var currentRank = pvpProfile->GetSeriesCurrentRank();
+
+        var job = Service.ClientState.LocalPlayer!.ClassJob.GameData!.Abbreviation;
+        var levelLabel = (GetAddonText(14860) ?? "Series Level").Trim().Replace(":", "");
+        var rank = currentRank > 30 ? 30 : currentRank; // 30 = Series Max Rank, hopefully in the future too
+        var level = rank.ToString().Aggregate("", (str, chr) => str + (char)(SeIconChar.Number0 + byte.Parse(chr.ToString())));
+        var star = currentRank > claimedRank ? '*' : ' ';
+        var requiredExperience = GetRow<PvPSeriesLevel>(currentRank)!.Unknown0;
+
+        leftText->SetText($"{job}  {levelLabel} {level}{star}   {pvpProfile->SeriesExperience}/{requiredExperience}");
+
+        gaugeBar->SetSecondaryValue(0); // rested experience bar
+
+        // max value is set to 10000 in AddonExp_OnSetup and we won't change that, so adjust
+        gaugeBar->SetValue((uint)(pvpProfile->SeriesExperience / (float)requiredExperience * 10000), 0, false);
+
+        if (!Config.DisableColorChanges)
+        {
+            // trying to make it look like the xp bar in the PvP Profile window and failing miserably. eh, good enough
+            nineGridNode->AtkResNode.MultiplyRed = 65;
+            nineGridNode->AtkResNode.MultiplyGreen = 35;
+        }
+        else
+        {
+            ResetColor(nineGridNode);
+        }
+
+        _isUpdatePending = false;
+    }
+
+    private void HandleSanctuaryBar(AtkNineGridNode* nineGridNode, Structs.AtkComponentGaugeBar* gaugeBar, AtkTextNode* leftText)
+    {
+        var mjiManager = MJIManager.Instance();
+        if (mjiManager == null)
+        {
+            ResetColor(nineGridNode);
+            return;
+        }
+
+        if (mjiManager->IslandState.CurrentRank > GetRowCount<MJIRank>() - 1)
+        {
+            ResetColor(nineGridNode);
+            return;
+        }
+
+        var job = Config.SanctuaryBarHideJob ? "" : Service.ClientState.LocalPlayer!.ClassJob.GameData!.Abbreviation + "  ";
+        var levelLabel = (GetAddonText(14252) ?? "Sanctuary Rank").Trim().Replace(":", "");
+        var level = mjiManager->IslandState.CurrentRank.ToString().Aggregate("", (str, chr) => str + (char)(SeIconChar.Number0 + byte.Parse(chr.ToString())));
+        var requiredExperience = GetRow<MJIRank>(mjiManager->IslandState.CurrentRank)!.ExpToNext;
+
+        var expStr = mjiManager->IslandState.CurrentXP.ToString();
+        var reqExpStr = requiredExperience.ToString();
+        if (requiredExperience == 0)
+        {
+            expStr = reqExpStr = "--";
+        }
+
+        leftText->SetText($"{job}{levelLabel} {level}   {expStr}/{reqExpStr}");
+
+        gaugeBar->SetSecondaryValue(0); // rested experience bar
+
+        // max value is set to 10000 in AddonExp_OnSetup and we won't change that, so adjust
+        gaugeBar->SetValue((uint)(mjiManager->IslandState.CurrentXP / (float)requiredExperience * 10000), 0, false);
+
+        if (!Config.DisableColorChanges)
+        {
+            // blue seems nice.. just like the sky ^_^
+            nineGridNode->AtkResNode.MultiplyRed = 25;
+            nineGridNode->AtkResNode.MultiplyGreen = 60;
+            nineGridNode->AtkResNode.MultiplyBlue = 255;
+        }
+        else
+        {
+            ResetColor(nineGridNode);
+        }
+
+        _isUpdatePending = false;
     }
 
     private void ResetColor(AtkNineGridNode* nineGridNode)
