df097bf8222accef4bce72cd0b5a08e82654e4e5
Haselnussbomber <mail@haselnussbomber.de>
2023-10-06 12:56:23 +0200

    maybe fix agenda search bar crash

--- HaselTweaks/Windows/MJICraftScheduleSettingSearchBar.cs
+++ HaselTweaks/Windows/MJICraftScheduleSettingSearchBar.cs
@@ -45,26 +45,27 @@ public unsafe class MJICraftScheduleSettingSearchBar : Window
 
         if (lastQuery != Query)
         {
-            var entries = new List<string>();
-            foreach (var ptr in Addon->TreeList->Items.Span)
+            var entries = new List<(uint Index, string ItemName)>();
+            for (var i = 0u; i < Addon->TreeList->Items.Size(); i++)
             {
-                var item = ptr.Value;
-                if (item != null)
+                var item = Addon->TreeList->Items.Get(i).Value;
+                if (item != null && item->Title != null)
                 {
                     var itemName = MemoryHelper.ReadStringNullTerminated(*(nint*)item->Title);
-                    entries.Add(itemName.ToLower());
+                    entries.Add((i, itemName.ToLower()));
                 }
             }
 
-            var result = FuzzySharp.Process.ExtractTop(Query.ToLower(), entries, (entry) => entry, limit: 1).First();
+            var result = FuzzySharp.Process.ExtractTop((Index: 0u, ItemName: Query.ToLower()), entries, (entry) => entry.ItemName, limit: 1).First();
             if (result.Index >= 0 && result.Index < entries.Count)
             {
-                var item = Addon->TreeList->GetItem((uint)result.Index);
+                var index = result.Value.Index;
+                var item = Addon->TreeList->GetItem(index);
                 if (item != null && item->Data->Type != AtkComponentTreeListItemType.CollapsibleGroupHeader)
                 {
                     var evt = stackalloc AtkEvent[1];
                     var a5 = stackalloc uint[4];
-                    a5[4] = (uint)result.Index;
+                    a5[4] = index;
                     Addon->AtkUnitBase.ReceiveEvent(AtkEventType.ListItemToggle, 1, evt, (nint)a5);
                 }
             }
