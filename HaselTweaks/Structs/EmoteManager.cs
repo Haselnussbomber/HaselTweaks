6fe9351e8e2dd43a2115e355762d327b20418f91
Haselnussbomber <mail@haselnussbomber.de>
2023-12-29 20:41:04 +0100

    use pointer instead of offset for EmoteManager

--- HaselTweaks/Structs/EmoteManager.cs
+++ HaselTweaks/Structs/EmoteManager.cs
@@ -1,12 +1,11 @@
-using FFXIVClientStructs.FFXIV.Client.Game;
-
 namespace HaselTweaks.Structs;
 
 // ctor "33 D2 C7 41 ?? ?? ?? ?? ?? 48 8D 05 ?? ?? ?? ?? 48 89 51 08 48 89 01 48 8B C1 48 89 51 10 48 89 51 18"
 [StructLayout(LayoutKind.Explicit, Size = 0x40)]
 public unsafe partial struct EmoteManager
 {
-    public static EmoteManager* Instance() => (EmoteManager*)((nint)GameMain.Instance() + 0x3FC0);
+    [StaticAddress("48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 40 84 ED 74 18", 3)]
+    public static partial EmoteManager* Instance();
 
     [MemberFunction("E8 ?? ?? ?? ?? 40 84 ED 74 18")]
     public readonly partial bool ExecuteEmote(ushort emoteId, nint targetData = 0);
