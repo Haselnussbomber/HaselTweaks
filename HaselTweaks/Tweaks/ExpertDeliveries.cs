9b638a2567bf9c9cc162f38b2e65a79555209c85
Haselnussbomber <mail@haselnussbomber.de>
2024-07-30 03:56:50 +0200

    prevent reset to the first expert delivery item

--- HaselTweaks/Tweaks/ExpertDeliveries.cs
+++ HaselTweaks/Tweaks/ExpertDeliveries.cs
@@ -1,11 +1,13 @@
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using HaselCommon.Services;
 using HaselTweaks.Enums;
 using HaselTweaks.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace HaselTweaks.Tweaks;
 
-public sealed unsafe class ExpertDeliveries(AddonObserver AddonObserver) : ITweak
+public sealed unsafe class ExpertDeliveries(ILogger<ExpertDeliveries> Logger, AddonObserver AddonObserver) : ITweak
 {
     public string InternalName => nameof(ExpertDeliveries);
     public TweakStatus Status { get; set; } = TweakStatus.Uninitialized;
@@ -40,6 +42,12 @@ public sealed unsafe class ExpertDeliveries(AddonObserver AddonObserver) : ITwea
 
         if (!TryGetAddon<AtkUnitBase>(addonName, out var addon))
             return;
+
+        // prevent item selection for controller users to reset to the first entry
+        if (*(short*)&AgentGrandCompanySupply.Instance()->SelectedTab == 2)
+            return;
+
+        Logger.LogDebug("Changing tab...");
 
         var atkEvent = new AtkEvent();
         addon->ReceiveEvent(AtkEventType.ButtonClick, 4, &atkEvent);
