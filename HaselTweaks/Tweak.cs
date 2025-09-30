8e98b54f049a056fcec1ce2fc37eebaf61f4ef9b
Haselnussbomber <mail@haselnussbomber.de>
2025-09-30 17:29:33 +0200

    Remove unnecessary code
    - No need for RunOnFrameworkThread. It's on the Framework thread
    because hosting starts on it.
    - GC.SuppressFinalize doesn't do anything without finalizer.
    - Fix logs printing tweak name twice.

--- HaselTweaks/Tweak.cs
+++ HaselTweaks/Tweak.cs
@@ -26,51 +26,49 @@ public abstract unsafe partial class Tweak : ITweak, IHostedService
         if (!_pluginConfig.EnabledTweaks.Contains(InternalName))
             return Task.CompletedTask;
 
-        return _framework.RunOnFrameworkThread(() =>
+        try
         {
-            try
-            {
-                OnEnable();
-                Status = TweakStatus.Enabled;
-            }
-            catch (SignatureException ex)
-            {
-                Status = TweakStatus.Outdated;
-                _logger.LogError(ex, "[{tweakName}] Error while enabling tweak", InternalName);
-            }
-            catch (KeyNotFoundException ex)
-            {
-                Status = TweakStatus.Outdated;
-                _logger.LogError(ex, "[{tweakName}] Error while enabling tweak", InternalName);
-            }
-            catch (Exception ex)
-            {
-                Status = TweakStatus.Error;
-                _logger.LogError(ex, "[{tweakName}] Error while enabling tweak", InternalName);
-            }
-        });
+            _logger.LogInformation("Enabling tweak");
+            OnEnable();
+            Status = TweakStatus.Enabled;
+        }
+        catch (SignatureException ex)
+        {
+            Status = TweakStatus.Outdated;
+            _logger.LogError(ex, "Error while enabling tweak");
+        }
+        catch (KeyNotFoundException ex)
+        {
+            Status = TweakStatus.Outdated;
+            _logger.LogError(ex, "Error while enabling tweak");
+        }
+        catch (Exception ex)
+        {
+            Status = TweakStatus.Error;
+            _logger.LogError(ex, "Error while enabling tweak");
+        }
+
+        return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken _)
     {
-        return _framework.RunOnFrameworkThread(() =>
+        try
         {
-            try
-            {
-                OnDisable();
-                Status = TweakStatus.Disabled;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "[{tweakName}] Error while disabling tweak", InternalName);
-            }
-        });
+            _logger.LogInformation("Disabling tweak");
+            OnDisable();
+            Status = TweakStatus.Disabled;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error while disabling tweak");
+        }
+
+        return Task.CompletedTask;
     }
 
     public void Dispose()
     {
-        GC.SuppressFinalize(this);
-
         if (Status is TweakStatus.Disposed or TweakStatus.Outdated)
             return;
 
