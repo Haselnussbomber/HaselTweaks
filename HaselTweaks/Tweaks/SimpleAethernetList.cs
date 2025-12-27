28f3c8ea4747b770cf1d5b11de5b93f3794322c5
Haselnussbomber <mail@haselnussbomber.de>
2025-12-27 03:04:47 +0100

    Rework SimpleAethernetList to use IAddonLifecycle more

--- HaselTweaks/Tweaks/SimpleAethernetList.cs
+++ HaselTweaks/Tweaks/SimpleAethernetList.cs
@@ -7,42 +7,42 @@ namespace HaselTweaks.Tweaks;
 [RegisterSingleton<IHostedService>(Duplicate = DuplicateStrategy.Append), AutoConstruct]
 public unsafe partial class SimpleAethernetList : Tweak
 {
-    private readonly IGameInteropProvider _gameInteropProvider;
-
-    private Hook<AddonTeleportTown.Delegates.ReceiveEvent>? _receiveEventHook;
+    private readonly IAddonLifecycle _addonLifecycle;
 
     public override void OnEnable()
     {
-        _receiveEventHook = _gameInteropProvider.HookFromAddress<AddonTeleportTown.Delegates.ReceiveEvent>(
-            AddonTeleportTown.StaticVirtualTablePointer->ReceiveEvent,
-            ReceiveEventDetour);
-        _receiveEventHook.Enable();
+        _addonLifecycle.RegisterListener(AddonEvent.PreReceiveEvent, "TelepotTown", OnPreReceiveEvent);
     }
 
     public override void OnDisable()
     {
-        _receiveEventHook?.Dispose();
-        _receiveEventHook = null;
+        _addonLifecycle.UnregisterListener(AddonEvent.PreReceiveEvent, "TelepotTown", OnPreReceiveEvent);
     }
 
-    private void ReceiveEventDetour(AddonTeleportTown* addon, AtkEventType eventType, int eventParam, AtkEvent* atkEvent, AtkEventData* atkEventData)
+    private void OnPreReceiveEvent(AddonEvent type, AddonArgs addonArgs)
     {
-        if (eventType == AtkEventType.ListItemRollOver)
-        {
-            var agent = AgentTelepotTown.Instance();
-            var index = atkEventData->ListItemData.SelectedIndex;
-            if (agent->Data != null && index >= 0)
-            {
-                var item = addon->List->GetItem(index);
-                if (item != null && item->UIntValues.LongCount >= 4)
-                {
-                    agent->Data->SelectedAetheryte = (byte)item->UIntValues[3];
-                    agent->Data->Flags |= 2;
-                    return;
-                }
-            }
-        }
+        if (addonArgs is not AddonReceiveEventArgs args)
+            return;
 
-        _receiveEventHook!.Original(addon, eventType, eventParam, atkEvent, atkEventData);
+        if ((AtkEventType)args.AtkEventType != AtkEventType.ListItemRollOver)
+            return;
+
+        var listItemData = (AtkEventData.AtkListItemData*)args.AtkEventData;
+        var index = listItemData->SelectedIndex;
+        if (index < 0)
+            return;
+
+        var addon = (AddonTeleportTown*)args.Addon.Address;
+        var item = addon->List->GetItem(index);
+        if (item == null || item->UIntValues.LongCount < 4)
+            return;
+
+        var agent = AgentTelepotTown.Instance();
+        if (agent->Data == null)
+            return;
+
+        agent->Data->SelectedAetheryte = (byte)item->UIntValues[3];
+        agent->Data->Flags |= 2;
+        listItemData->SelectedIndex = -1; // suppress original handling of this event
     }
 }
