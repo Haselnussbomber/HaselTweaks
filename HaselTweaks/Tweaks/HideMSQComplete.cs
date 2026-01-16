b6068f1af0beab18caf22832e4ab00f9838412ee
Haselnussbomber <mail@haselnussbomber.de>
2026-01-16 09:47:55 +0100

    Better pad input support for HideMSQComplete

--- HaselTweaks/Tweaks/HideMSQComplete.cs
+++ HaselTweaks/Tweaks/HideMSQComplete.cs
@@ -19,7 +19,7 @@ public unsafe partial class HideMSQComplete : Tweak
         _addonLifecycle.UnregisterListener(AddonEvent.PostRefresh, "ScenarioTree", ScenarioTree_PostRefresh);
 
         if (Status is TweakStatus.Enabled)
-            UpdateVisibility(true);
+            UpdateVisibility(true, true);
     }
 
     private void ScenarioTree_PostRefresh(AddonEvent type, AddonArgs args)
@@ -29,17 +29,32 @@ public unsafe partial class HideMSQComplete : Tweak
 
     private static void Update()
     {
-        var agentScenarioTree = AgentScenarioTree.Instance();
-        UpdateVisibility(agentScenarioTree->Data != null && agentScenarioTree->Data->CurrentScenarioQuest != 0);
+        var agent = AgentScenarioTree.Instance();
+        if (agent->Data == null)
+            return;
+
+        var data = agent->Data;
+        var hasMainQuest = data->MainScenarioQuestIds[data->MSQPathIndex] != 0;
+        var hasJobQuest = data->JobQuestIds[data->JobQuestIndex] != 0;
+        UpdateVisibility(hasMainQuest, hasJobQuest);
     }
 
-    private static void UpdateVisibility(bool visible)
+    private static void UpdateVisibility(bool hasMainQuest, bool hasJobQuest)
     {
         if (!TryGetAddon<AtkUnitBase>(AgentId.ScenarioTree, out var addon))
             return;
 
-        addon->GetNodeById(11)->ToggleVisibility(visible); // AtkTextNode
-        addon->GetNodeById(12)->ToggleVisibility(visible); // AtkNineGridNode
-        addon->GetNodeById(13)->ToggleVisibility(visible); // AtkComponentButton
+        addon->GetNodeById(11)->ToggleVisibility(hasMainQuest); // AtkTextNode (title)
+        addon->GetNodeById(12)->ToggleVisibility(hasMainQuest); // AtkNineGridNode (title background)
+        addon->GetNodeById(13)->ToggleVisibility(hasMainQuest); // AtkComponentButton (quest button)
+
+        var lowerBox = addon->GetNodeById(6);
+        var isInputHintShown = !hasMainQuest && lowerBox != null && lowerBox->Timeline != null && lowerBox->Timeline->ActiveLabelId == 102;
+        addon->GetNodeById(9)->ToggleVisibility(!isInputHintShown); // AtkResNode (hide input hint)
+
+        if (!hasMainQuest && !hasJobQuest)
+            addon->Flags1A0 |= 0b1000_0000; // disable focusability
+        else
+            addon->Flags1A0 &= unchecked((byte)~0b1000_0000); // enable focusability
     }
 }
