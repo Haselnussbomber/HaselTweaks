85823f0e3592a0156ead2fb71487e04b456595e0
Haselnussbomber <mail@haselnussbomber.de>
2026-06-01 17:25:18 +0200

    Use primary constructor

--- HaselTweaks/Records/CharaSelectCharacter.cs
+++ HaselTweaks/Records/CharaSelectCharacter.cs
@@ -3,18 +3,10 @@ using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 
 namespace HaselTweaks.Records;
 
-public unsafe class CharaSelectCharacter
+public unsafe class CharaSelectCharacter(Character* character, CharaSelectCharacterEntry* entry)
 {
-    public Character* Character { get; }
-    public ulong ContentId { get; }
-    public ushort TerritoryType { get; }
-    public byte ClassJobId { get; }
-
-    public CharaSelectCharacter(Character* character, CharaSelectCharacterEntry* entry)
-    {
-        Character = character;
-        ContentId = entry->ContentId;
-        TerritoryType = entry->ClientSelectData.TerritoryType;
-        ClassJobId = entry->ClientSelectData.CurrentClass;
-    }
+    public Character* Character { get; } = character;
+    public ulong ContentId { get; } = entry->ContentId;
+    public ushort TerritoryType { get; } = entry->ClientSelectData.TerritoryType;
+    public byte ClassJobId { get; } = entry->ClientSelectData.CurrentClass;
 }
